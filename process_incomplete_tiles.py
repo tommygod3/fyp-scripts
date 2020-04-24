69e1b193d67ccec4994556d11f6a9fef07d97e8e
tommygod3 <tommygod1@live.com>
2020-04-24 17:39:42 +0100

    Added missing import

--- process_incomplete_tiles.py
+++ process_incomplete_tiles.py
@@ -1,4 +1,4 @@
-import json, subprocess, glob, sys
+import json, subprocess, glob, sys, os
 import pathlib, argparse
 
 # load environment config and set path vars
@@ -6,7 +6,7 @@ file_path = os.path.realpath(__file__)
 directory_path = "/".join(file_path.split("/")[:-1])
 with open(f"{directory_path}/environment.json") as reader:
     environment = json.load(reader)
-    
+
 def process_all(top_directory):
     for directory in glob.iglob(f"{top_directory}/*"):
         subprocess.call(f"bsub -o {directory}/%J.out -W 5:00 -q short-serial {sys.executable} {directory_path}/process_tile.py -d {directory}/", shell=True)
